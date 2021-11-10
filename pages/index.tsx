34498056a0e51fe3f05c264777cece2bfe678b0a
Izhan <babuizhan1234@gmail.com>
2021-11-10 20:08:07 +0530

    added images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage, GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = (props) => {
